79cafd3a6d8770da97f8a86d11f01f55b5b5ae27
doraderin <dora.tasci@hisarschool.k12.tr>
2024-12-10 18:51:40 +0300

    plotthegrapg method

--- calculators/script.js
+++ calculators/script.js
@@ -16,35 +16,25 @@ function drawGraph() {
     const endX = canvas.width / 2;
     const step = 0.01;
 
-    ctx.beginPath();
-    for (let x = startX; x <= endX; x += step) {
-        const y = evaluateExpression(expr1, x); 
-        const screenX = x + canvas.width / 2;
-        const screenY = -y + canvas.height / 2;
+    plotTheGraph(expr1, "violet")
+    plotTheGraph(expr2, "#7a80e9")
 
-        if (x === startX) {
-            ctx.moveTo(screenX, screenY);
-        } else {
-            ctx.lineTo(screenX, screenY);
+    function plotTheGraph(expr, lineColor){
+        ctx.beginPath();
+        for (let x = startX; x <= endX; x += step) {
+            const y = evaluateExpression(expr, x); 
+            const screenX = x + canvas.width / 2;
+            const screenY = -y + canvas.height / 2;
+    
+            if (x === startX) {
+                ctx.moveTo(screenX, screenY);
+            } else {
+                ctx.lineTo(screenX, screenY);
+            }
         }
+        ctx.strokeStyle = lineColor;  
+        ctx.stroke();
     }
-    ctx.strokeStyle = "violet";  
-    ctx.stroke();
-
-    ctx.beginPath();
-    for (let x = startX; x <= endX; x += step) {
-        const y = evaluateExpression(expr2, x);  
-        const screenX = x + canvas.width / 2;
-        const screenY = -y + canvas.height / 2;
-
-        if (x === startX) {
-            ctx.moveTo(screenX, screenY);
-        } else {
-            ctx.lineTo(screenX, screenY);
-        }
-    }
-    ctx.strokeStyle = "#7a80e9 "; 
-    ctx.stroke();
 }
 
 function evaluateExpression(expr, x) {
